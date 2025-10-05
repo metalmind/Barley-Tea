3fa5fbd98851092484bb45d66fc3be6d7c519bd2
Isaac Kehler <caasik23@gmail.com>
2025-10-05 00:03:08 -0700

    Set up Tailwind.css and Implemented a navbar skeleton
    Set up tailwindcss and added a bottom navbar component that I call on each page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import BottomNavbar from './components/BottomNavbar'
 import './App.css'
 
+/**
+ * Calls the bottom navbar on all pages
+ */
 function App() {
   return (
     <Router>
+      <BottomNavbar />
+      
       <Routes>
-        <Route path="/" element={<div>hi</div>} />
-        <Route path="/highlights" element={<div>hello there</div>} />
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/search" element={<div>Search Page</div>} />
+        <Route path="/social" element={<div>Social Page</div>} />
+        <Route path="/highlights" element={<div>Highlights Page</div>} />
       </Routes>
     </Router>
   )
