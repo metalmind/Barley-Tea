bf05515d0f425bc3fe59b4e2ace82146844260cb
JonnyTwist <jsatwist@hotmail.com>
2025-10-05 08:12:14 -0700

    Added a spinny part to the week view. Alison is going to hate my CSS

--- src/views/WeeklyNotes.tsx
+++ src/views/WeeklyNotes.tsx
@@ -1,25 +1,42 @@
 import { Note } from "../components/Note/Note";
 import { useNotes } from "../hooks/useNotes";
+import "./WeeklyView.css";
 
 export const NoteList = () => {
-  const { notes, loading } = useNotes();
+    const { notes, loading } = useNotes();
 
-  if (loading) return <p>Loading notes...</p>;
+    if (loading) return <p>Loading notes...</p>;
 
-  return (
-    <>
-      {notes.length === 0 ? (
-        <p>No notes yet!</p>
-      ) : (
-        notes.map((note) => (
-          <Note 
-            id={note.id}
-            key={note.id}
-            content={note.message}
-            type={note.background}
-          />
-        ))
-      )}
-    </>
-  );
+    return (
+        // <div id="page">
+        //   {notes.length === 0 ? (
+        //     <p>No notes yet!</p>
+        //   ) : (
+        //     notes.map((note) => (
+        //       <Note 
+        //         id={note.id}
+        //         key={note.id}
+        //         content={note.message}
+        //         type={note.background}
+        //       />
+        //     ))
+        //   )}
+        // </div>
+
+        <div id="weeklyViewPage">
+            {notes.length === 0 ? (
+                <p>No notes yet!</p>
+            ) : (
+                notes.map((note) => (
+                    <Note
+                        id={note.id}
+                        key={note.id}
+                        content={note.message + note.created_at}
+                        type={note.background}
+                    />
+                ))
+            )}
+        </div>
+
+    );
 };
